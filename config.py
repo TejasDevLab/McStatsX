cd06fe69d6b9296e6ed0ed73ebef5b567113674e
Tejas Chauhan <142246988+dev-tejas@users.noreply.github.com>
2023-10-08 12:23:07 +0530

    Update and rename config.json to config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Discord Bot Token
+TOKEN = 'YOUR_DISCORD_BOT_TOKEN'
+
+# Minecraft Server Configuration
+SERVER_IP = 'mc.hypixel.net'
+SERVER_PORT = 25565
+
+# Discord Channel ID
+CHANNEL_ID = 'YOUR_CHANNEL_ID'
+
+# Emojis for Online and Offline Status
+ONLINE_EMOJI = '🟢'
+OFFLINE_EMOJI = '🔴'
